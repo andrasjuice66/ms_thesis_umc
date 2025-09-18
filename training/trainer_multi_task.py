65f7e77ee6dfbf58581b8705ac6ad6e48deb90a8
andrasjuice <cackles_gloomy0k@icloud.com>
2025-09-18 14:53:17 +0200

    merge

--- training/trainer_multi_task.py
+++ training/trainer_multi_task.py
@@ -153,7 +153,12 @@ class MultiTaskTrainer:
             
             # Always compute age loss
             print("age_preds, age_gts", age_preds, age_gts)
-            
+            print("age_preds.dtype, age_gts.dtype", age_preds.dtype, age_gts.dtype)
+            age_preds = age_preds.float()
+            age_gts = age_gts.float()
+            print("age_preds, age_gts", age_preds, age_gts)
+            print("age_preds.dtype, age_gts.dtype", age_preds.dtype, age_gts.dtype)
+
             age_loss = self.age_criterion(age_preds, age_gts)
             
             # Check if we're in age-only mode
